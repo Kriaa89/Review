2448180e82bcc8c76ac86411a1d6bdbf02ab1378
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 16:38:15 +0100

    Add Header component with navigation link to create posts

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    <header>
+        <div className='Header'>
+            <Link to='/'>
+                <h2>Create Posts</h2>
+            </Link>
+        </div>
+    </header>
+}
