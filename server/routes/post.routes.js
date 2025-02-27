20abc4f365d1dfda830a89ee5ef208538fb8a49e
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 14:22:26 +0100

    Implement CRUD routes for posts in post.routes.js

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express"; // import Router From express module
-import * as postController from "../controllers/post.controller.js" // import all the function from controller
+import { CreatePost, GetAllBooks, GetOneBooks, UpdateBook, DeleteBook } from "../controllers/post.controller.js" // import all the function from controller
 
 
 const router = Router(); // create A NEW ROUTER
 
-
+router.post("/", CreatePost);
+router.get("/", GetAllBooks);
+router.get("/:id", GetOneBooks);
+router.put("/:id", UpdateBook);
+router.delete("/:id", DeleteBook)
