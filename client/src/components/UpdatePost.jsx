1317a6a1a78880ceebcb3e42dfde5fb16e75b9bc
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 18:39:02 +0100

    Implement form handling for UpdatePost component with input fields for title, description, and reviews

--- client/src/components/UpdatePost.jsx
+++ client/src/components/UpdatePost.jsx
@@ -21,5 +21,28 @@ const UpdatePost = () => {
         axios.put("http://localhost:8000/api/posts/" + id, post)
             .then(() => navigate("/"))
             .catch(err => setErrors(err.response.data.errors));
-    }
+    };
+
+    return (
+        <div>
+            <h2>Edit</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Post Title:</label>
+                    <input type="text" value={post.title} onChange={e => setPost({...post, title: e.target.value})}> </input>
+                    {errors.title && <span>{errors.title.message}</span>}
+                </div>
+                <div>
+                    <label>Post Description:</label>
+                    <input type="text" value={post.description} onChange={e => setPost({...post, description: e.target.value})}></input>
+                    {errors.description && <span>{errors.description.message}</span>}
+                </div>
+                <div>
+                    <label>Add a review:</label>
+                    <input type="text" value={post.reviews} onChange={e => setPost({...post, reviews: e.target.value})}></input>
+                </div>
+            </form>
+        </div>
+    );
 }
+export default UpdatePost;
