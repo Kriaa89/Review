2ec68170f58db41c35520d7d415700b6a2d68f94
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 14:02:12 +0100

    Add error handling to post creation in post controller

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -2,5 +2,10 @@ import Post from "../model/post.model";
 
 // create a new post and save it to the database
 async function CreatePost(req, res) {
-    
+    try {
+        const NewPost = await Post.create(req.body)
+        res.json(NewPost);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
