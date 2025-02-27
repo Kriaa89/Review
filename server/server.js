5d8048ed11014ad0ac4a522601bf454645fde879
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 14:31:16 +0100

    Add CORS and dotenv imports in server.js for configuration

--- server/server.js
+++ server/server.js
@@ -1,4 +1,17 @@
 import express from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+import dbConnect from
+
+
+
+
+
+
+
+
 const app = express();
 const port = 8000;
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
