68067dc5406e5fa706774fcb0600f65962d0f9be
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 18:23:18 +0100

    Add header title to Home page and start UpdatePost component implementation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
     return (
         <header>
         <div className='Header'>
+            <h1>Home</h1>
             <Link to='/posts/new'>
                 <h2>Create Posts</h2>
             </Link>
---
+++ client/src/components/UpdatePost.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const UpdatePost = () => {
+    const 
+}
