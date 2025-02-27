6a7fc0de1961ea730bc404570669f4f4c4fa5a9f
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 18:28:42 +0100

    Implement UpdatePost component with form handling and API integration

--- client/src/components/UpdatePost.jsx
+++ client/src/components/UpdatePost.jsx
@@ -3,5 +3,23 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const UpdatePost = () => {
-    const 
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [ post, setPost ] = useState ({
+        title : "",
+        description : "",
+        reviews : []
+    });
+    const [errors, setErrors] = useState({});
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/posts/" + id)
+            .then(res => setPost(res.data))
+            .catch(err => console.log(err));
+    }, [id]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.put("http://localhost:8000/api/posts/" + id, post)
+            .then(() => navigate("/"))
+            .catch(err => setErrors(err.response.data.errors));
+    }
 }
