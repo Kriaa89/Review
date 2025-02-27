6c9a0ada200fdeffacdf70329236e755ea39cde9
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 18:57:34 +0100

    Fix PostDetails component to display post description and reviews correctly

--- client/src/components/PostDetails.jsx
+++ client/src/components/PostDetails.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react"; // import UseState and UseEffect to use hooks in the function
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 
 const PostDetails = () => {
@@ -21,10 +21,18 @@ const PostDetails = () => {
             <h3>Reviews {post.reviews.index}</h3>
             <div>
                 <p><strong>Title:</strong> {post.title}</p>
-                <p><strong>Post Description:</strong></p>
-                <p><strong>Title</strong></p>
+                <p><strong>Post Description:</strong> {post.description}</p>
+            </div>
+            <div>
+                <h4>Reviews</h4>
+                <ul>
+                    {post.reviews.map((review, index) => (
+                        <li key={index}>{review}</li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
 
 }
+export default PostDetails; // export the function to be used in other files
