e57b96cd0c00ef5ce252b4f57d553dfd0ca5815f
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-27 16:52:37 +0100

    Add PostForm component and update Header for navigation to create posts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    <header>
+    return (
+        <header>
         <div className='Header'>
-            <Link to='/'>
+            <Link to='/posts/new'>
                 <h2>Create Posts</h2>
             </Link>
         </div>
     </header>
+    );
 }
+export default Header;
---
+++ client/src/components/PostForm.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const PostForm = () => {
+    const [post, setPost] = useState({
+        title : "",
+        description : "",
+        reviews : []
+    });
+    const [errors, setErrors] = useState({});
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:8000/api/posts", post)
+            .then(() => navigate("/"))
+            .catch(err => setErrors(err.response.data.errors));
+    };
+    return (
+        <div>
+            <h2>Add a Post</h2>
+        </div>
+    )
+}
