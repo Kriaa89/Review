4c321cb4aadb1e9f6d0a729c5e2cbad43f0f831f
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-28 08:49:59 +0100

    Add component imports to App for improved structure and functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
-import { useState } from 'react'
+import Header from './components/Header'
+import PostForm from './components/PostForm'
+import PostList from './components/PostList'
+import PostDetail from './components/PostDetails'
+import UpdatePost from './components/UpdatePost'
 import './App.css'
 
 function App() {
